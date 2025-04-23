17bace15d84310ba7409df18e1193335b6126ac2
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-04-23 10:17:09 -0700

    Fix hidden skip to content button

--- src/components/common/SkipToContent.tsx
+++ src/components/common/SkipToContent.tsx
@@ -5,7 +5,7 @@ export const SkipToContent = () => {
   return (
     <AnchorButton
       href={`#${ELEMENT_IDS.MAIN_CONTENT}`}
-      className="absolute left-1/2 top-0 z-50 min-w-max -translate-x-1/2 -translate-y-full rounded-t-none opacity-0 transition-all focus-visible:translate-y-0 focus-visible:opacity-100"
+      className="z-skip-to-content absolute left-1/2 top-0 min-w-max -translate-x-1/2 -translate-y-full rounded-t-none opacity-0 transition-all focus-visible:translate-y-0 focus-visible:opacity-100"
     >
       Skip to Content
     </AnchorButton>
