948102374840cfbd8fb88140b5aecc527a1d6b92
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-04-25 12:24:30 -0700

    Change icons for clarity

--- src/components/entryCard/entryControls/EntryControls.tsx
+++ src/components/entryCard/entryControls/EntryControls.tsx
@@ -1,5 +1,5 @@
 import { faStar } from "@fortawesome/free-regular-svg-icons";
-import { faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { EntryControlButton } from "./EntryControlButton";
 
 interface EntryControlsProps {}
@@ -8,7 +8,7 @@ export const EntryControls = ({}: EntryControlsProps) => {
   return (
     <div className="text-accent flex flex-col items-center gap-0.5 p-2">
       <EntryControlButton icon={faStar} />
-      <EntryControlButton icon={faPen} />
+      <EntryControlButton icon={faEdit} />
       <EntryControlButton icon={faTrash} />
     </div>
   );
