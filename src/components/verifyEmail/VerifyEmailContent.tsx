d82bb4921df9a60c5588c725289f330e8e1b09fc
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-05-19 20:13:40 -0700

    Implement verify email form with new form system

--- src/components/verifyEmail/VerifyEmailContent.tsx
+++ src/components/verifyEmail/VerifyEmailContent.tsx
@@ -1,70 +1,15 @@
 "use client";
 
-import { ChangeEventHandler, MouseEventHandler, useState } from "react";
-import { useSearchParams } from "next/navigation";
-
-import { createClientCS } from "@/lib/db/supabase/client";
-import { routes } from "@/routes/routes";
-import { getRedirectUrl } from "@/utils/urls";
-
-import { Button } from "../shared/buttons/Button";
+import { VerifyEmailForm } from "./VerifyEmailForm";
 import { ELEMENT_IDS } from "../shared/constants/elementIds";
 
-/**
- * TODO: handle email already verified error
- * TODO: handle email doesn't exist error(?)
- */
 export const VerifyEmailContent = () => {
-  const searchParams = useSearchParams();
-  const supabase = createClientCS();
-
-  // TODO: implement proper form fields with validations
-  const [formValues, setFormValues] = useState({
-    email: searchParams.get("email") ?? "",
-  });
-
-  const resendVerification: MouseEventHandler<HTMLButtonElement> = async (
-    e,
-  ) => {
-    e.preventDefault();
-
-    const { error } = await supabase.auth.resend({
-      type: "signup",
-      email: formValues.email,
-      options: {
-        emailRedirectTo: getRedirectUrl(routes.search()),
-      },
-    });
-
-    // TODO: add better error handling
-    // TODO: remove test logs
-    if (error) {
-      console.error(error);
-    }
-  };
-
-  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setFormValues((prev) => ({
-      ...prev,
-      [e.target.name]: e.target.value,
-    }));
-  };
-
   return (
-    <main id={ELEMENT_IDS.MAIN_CONTENT}>
-      <form className="flex flex-col">
-        {"Didn't"} receive an email?
-        <input
-          type="email"
-          name="email"
-          value={formValues.email}
-          placeholder="Email"
-          onChange={handleChange}
-        />
-        <Button type="submit" onClick={resendVerification}>
-          Resend
-        </Button>
-      </form>
+    <main id={ELEMENT_IDS.MAIN_CONTENT} className="flex flex-col items-center">
+      <div className="w-full max-w-md">
+        <p>{"Didn't"} receive an email?</p>
+        <VerifyEmailForm />
+      </div>
     </main>
   );
 };
---
+++ src/components/verifyEmail/VerifyEmailForm/index.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { FormProvider, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter, useSearchParams } from "next/navigation";
+
+import { createClientCS } from "@/lib/db/supabase/client";
+import { routes } from "@/routes/routes";
+import { getRedirectUrl } from "@/utils/urls";
+
+import { VerifyEmailFormValues, verifyEmailSchema } from "./schema";
+import { Button } from "../../shared/buttons/Button";
+import { RHFTextField } from "../../shared/formFields/RHFFields/RHFTextField";
+
+export const VerifyEmailForm = () => {
+  const supabase = createClientCS();
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const methods = useForm<VerifyEmailFormValues>({
+    defaultValues: { email: searchParams.get("email") ?? "" },
+    resolver: zodResolver(verifyEmailSchema),
+  });
+
+  const onSubmit = async (data: VerifyEmailFormValues) => {
+    const { error } = await supabase.auth.resend({
+      type: "signup",
+      email: data.email,
+      options: {
+        emailRedirectTo: getRedirectUrl(routes.search()),
+      },
+    });
+
+    // TODO: handle email already verified error
+    // TODO: handle email doesn't exist error(?)
+    // TODO: add better error handling
+    // TODO: remove test logs
+    if (error) {
+      console.error(error);
+    } else {
+      console.log("redirecting...");
+      router.push(routes.search());
+    }
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)}>
+        <div className="mb-5 w-full space-y-4">
+          <RHFTextField type="email" name="email" label="Email" required />
+        </div>
+        <Button type="submit" fullWidth>
+          Resend
+        </Button>
+      </form>
+    </FormProvider>
+  );
+};
