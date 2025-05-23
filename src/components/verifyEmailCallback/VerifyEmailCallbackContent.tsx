d8f5644e359d7eed28f368f477083e85e854e3d8
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-05-22 17:35:29 -0700

    Add copy for different verification states

--- src/components/verifyEmailCallback/VerifyEmailCallbackContent.tsx
+++ src/components/verifyEmailCallback/VerifyEmailCallbackContent.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import { QUERY_KEYS } from "@/constants/queryKeys";
 import { getSessionCS } from "@/lib/db/supabase/client";
 import { routes } from "@/routes/routes";
 
+import { VerificationStatus } from "./types";
 import { ELEMENT_IDS } from "../../constants/elementIds";
+import { VerifyEmailForm } from "../verifyEmail/VerifyEmailForm";
 
 /**
  * User will be authenticated before being redirected based on authentication.
@@ -16,21 +18,46 @@ import { ELEMENT_IDS } from "../../constants/elementIds";
 export const VerifyEmailCallbackContent = () => {
   const searchParams = useSearchParams();
   const router = useRouter();
+  const [status, setStatus] = useState<VerificationStatus>("verifying");
 
   useEffect(() => {
     getSessionCS().then((session) => {
-      const redirectUrl =
-        searchParams.get(QUERY_KEYS.REDIRECT_URL) ?? routes.search();
-
-      router.replace(session && redirectUrl ? redirectUrl : routes.home());
+      if (session) {
+        const redirectUrl =
+          searchParams.get(QUERY_KEYS.REDIRECT_URL) ?? routes.search();
+        router.replace(redirectUrl);
+      } else {
+        setStatus("invalid");
+      }
     });
   }, [searchParams, router]);
 
   return (
     <main id={ELEMENT_IDS.MAIN_CONTENT} className="flex flex-col items-center">
       <div className="w-full max-w-md">
-        <p>Verifying...</p>
-        <p>Hang on tight while we log you in.</p>
+        {status === "verifying" && (
+          <>
+            <p>Verifying...</p>
+            <p>Hang on tight while we log you in.</p>
+          </>
+        )}
+
+        {status === "invalid" && (
+          <>
+            <p className="text-negative-text font-semibold">
+              Verification link has expired or is invalid.
+            </p>
+            <p>Please request a new verification email.</p>
+            <VerifyEmailForm />
+          </>
+        )}
+
+        {status === "resent" && (
+          <>
+            <p>Verification link has been sent.</p>
+            <p>Please allow up to one minute to receive the link.</p>
+          </>
+        )}
       </div>
     </main>
   );
