05fb5ab881e9730708ded045b300052cb49d9156
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-05-27 14:14:47 -0700

    Fix drizzle configuration

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+const connectionString = process.env.DATABASE_URL;
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
+export default defineConfig({
+  out: "./drizzle",
+  schema: "./src/db/schema/*",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: connectionString,
+  },
+});
