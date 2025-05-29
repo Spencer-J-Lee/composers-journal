d685fac5ade5839e343ce10eee4bc8c421be38ee
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-05-28 18:03:38 -0700

    Use custom fetch for fetching entries

--- src/services/entries/get.ts
+++ src/services/entries/get.ts
@@ -1,13 +1,6 @@
-import { ERROR_MESSAGES } from "@/constants/messages";
-
 import { API_PATHS } from "../constants/apiPaths";
+import { fetchWithErrorHandling } from "../utils/fetchWithErrorHandling";
 
 export const apiGetEntries = async () => {
-  const res = await fetch(API_PATHS.ENTRIES.ROOT);
-
-  if (!res.ok) {
-    throw new Error(ERROR_MESSAGES.USER.FETCH.ENTRIES);
-  }
-
-  return res.json();
+  return await fetchWithErrorHandling(API_PATHS.ENTRIES.ROOT);
 };
