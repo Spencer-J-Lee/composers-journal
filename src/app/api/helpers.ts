d217a469633e0619362b27984a95904a2223b375
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-06-10 18:41:49 -0700

    Create val get helpers for api

---
+++ src/app/api/helpers.ts
@@ -0,0 +1,23 @@
+// TODO: refactor api to use these
+
+export const getQueryString = (params: URLSearchParams, key: string) => {
+  return params.get(key) ?? undefined;
+};
+
+export const getQueryInt = (params: URLSearchParams, key: string) => {
+  const val = params.get(key);
+
+  if (val === null) {
+    return undefined;
+  }
+
+  const parsed = parseInt(val);
+  return isNaN(parsed) ? undefined : parsed;
+};
+
+export const getQueryValue = <T>(
+  params: URLSearchParams,
+  key: string,
+): T | undefined => {
+  return (params.get(key) as T) ?? undefined;
+};
