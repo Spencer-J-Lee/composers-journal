e59e6df960f65b155a7736fd57468c30aa02c4b2
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-06-11 18:12:20 -0700

    Enable Divider to be used inside flexboxes

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -3,17 +3,27 @@ import clsx from "clsx";
 type DividerProps = {
   orientation?: "horizontal" | "vertical";
   className?: string;
+  flexChild?: boolean;
 };
 
 export const Divider = ({
   className,
   orientation = "horizontal",
+  flexChild,
 }: DividerProps) => {
   if (orientation === "horizontal") {
     return <hr className={clsx("border-border w-full border-b", className)} />;
   }
 
   if (orientation === "vertical") {
-    return <div className={clsx("border-border h-full border-l", className)} />;
+    return (
+      <div
+        className={clsx(
+          "border-border border-l",
+          flexChild ? "self-stretch" : "h-full",
+          className,
+        )}
+      />
+    );
   }
 };
