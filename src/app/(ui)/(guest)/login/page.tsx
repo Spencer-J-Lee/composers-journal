33e7519f4c74f50bd90cdef7d81ec17fa620c90c
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-06-16 16:39:46 -0700

    Remove unwanted text

--- src/app/(ui)/(guest)/login/page.tsx
+++ src/app/(ui)/(guest)/login/page.tsx
@@ -7,7 +7,7 @@ import { LoginContent } from "@/modules/login/components/LoginContent";
 const LoginPage = () => {
   return (
     <PageWrapper maxWidth="md">
-      <LoginContent />;
+      <LoginContent />
     </PageWrapper>
   );
 };
