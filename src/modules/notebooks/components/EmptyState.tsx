f2eac5fa68d840c58a960e249ce5cea4559520ff
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-06-21 18:35:30 -0700

    Create reusable empty state component

---
+++ src/modules/notebooks/components/EmptyState.tsx
@@ -0,0 +1,40 @@
+import { ReactNode } from "react";
+import Image from "next/image";
+
+import { Typography } from "@/components/Typography";
+
+export type EmptyStateProps = {
+  imageData: {
+    src: string;
+    alt: string;
+  };
+  title: string;
+  description: string;
+  children?: ReactNode;
+};
+
+export const EmptyState = ({
+  imageData: { src, alt },
+  title,
+  description,
+  children,
+}: EmptyStateProps) => {
+  return (
+    <div className="flex flex-col items-center">
+      <Image
+        src={src}
+        alt={alt}
+        width={200}
+        height={200}
+        style={{ objectFit: "cover" }}
+        className="mb-3"
+      />
+      <Typography variant="h2" className="mb-2">
+        {title}
+      </Typography>
+      <Typography variant="body">{description}</Typography>
+
+      {children && <div className="mt-6">{children}</div>}
+    </div>
+  );
+};
