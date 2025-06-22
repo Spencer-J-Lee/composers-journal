5912d8e10bc311f7862fc8138754bca87108c2aa
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-06-21 18:22:24 -0700

    Make sure only active notebooks are fetched

--- src/app/(ui)/(protected)/workspace/notebooks/page.tsx
+++ src/app/(ui)/(protected)/workspace/notebooks/page.tsx
@@ -1,11 +1,15 @@
 import { dbGetNotebooks } from "@/db/queries/notebooks";
+import { STATUSES } from "@/models/types/status";
 import { NotebooksContent } from "@/modules/notebooks/list/NotebooksContent";
 import { NotebooksEmptyCTA } from "@/modules/notebooks/list/NotebooksEmptyCTA";
 import { getUserOrRedirect } from "@/utils/getUserOrRedirect";
 
 const NotebooksPage = async () => {
   const user = await getUserOrRedirect();
-  const notebooks = await dbGetNotebooks({ ownerId: user.id });
+  const notebooks = await dbGetNotebooks({
+    ownerId: user.id,
+    status: STATUSES.ACTIVE,
+  });
 
   if (!notebooks.length) {
     return <NotebooksEmptyCTA />;
