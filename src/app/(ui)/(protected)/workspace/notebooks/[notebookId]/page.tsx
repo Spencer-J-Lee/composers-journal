e8e6e3e5130648c31b67c283c8023c36bebc5c2e
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-06-21 18:35:47 -0700

    Implement empty notebook CTA feature

--- src/app/(ui)/(protected)/workspace/notebooks/[notebookId]/page.tsx
+++ src/app/(ui)/(protected)/workspace/notebooks/[notebookId]/page.tsx
@@ -1,6 +1,10 @@
 import { notFound } from "next/navigation";
 
+import { dbGetEntries } from "@/db/queries/entries";
+import { STATUSES } from "@/models/types/status";
 import { NotebookContent } from "@/modules/notebooks/detail/NotebookContent";
+import { NotebookEmptyCTA } from "@/modules/notebooks/detail/NotebookEmptyCTA";
+import { getUserOrRedirect } from "@/utils/getUserOrRedirect";
 
 type NotebookPageProps = {
   params: { notebookId: string };
@@ -12,6 +16,17 @@ const NotebookPage = async ({ params }: NotebookPageProps) => {
 
   if (isNaN(parsedNotebookId)) {
     notFound();
+  }
+
+  const user = await getUserOrRedirect();
+  const entries = await dbGetEntries({
+    ownerId: user.id,
+    notebookId: parsedNotebookId,
+    status: STATUSES.ACTIVE,
+  });
+
+  if (!entries.length) {
+    return <NotebookEmptyCTA notebookId={parsedNotebookId} />;
   }
 
   return <NotebookContent notebookId={parsedNotebookId} />;
---
+++ src/modules/notebooks/detail/NotebookEmptyCTA.tsx
@@ -0,0 +1,28 @@
+import { LinkButton } from "@/components/buttons/LinkButton";
+import { ASSET_PATHS } from "@/constants/assetPaths";
+import { routes } from "@/constants/routes";
+
+import { EmptyState } from "../components/EmptyState";
+
+export type NotebookEmptyCTAProps = {
+  notebookId: number;
+};
+
+export const NotebookEmptyCTA = ({ notebookId }: NotebookEmptyCTAProps) => {
+  return (
+    <div className="flex min-h-full w-full items-center justify-center">
+      <EmptyState
+        imageData={{
+          src: ASSET_PATHS.EMPTY_STATE_WRITING,
+          alt: "Cartoon character writing lines with giant pencil.",
+        }}
+        title="No entries yet"
+        description="Let's start by creating a new one"
+      >
+        <LinkButton href={routes.entryCreate(notebookId)}>
+          Get Started
+        </LinkButton>
+      </EmptyState>
+    </div>
+  );
+};
