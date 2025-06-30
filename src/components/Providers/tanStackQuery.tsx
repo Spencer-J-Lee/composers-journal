c2a0a0f8151f317a39c7983f7d639bebc234227c
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-06-30 16:01:27 -0700

    Improve component description

--- src/components/Providers/tanStackQuery.tsx
+++ src/components/Providers/tanStackQuery.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ReactNode } from "react";
-import { QueryClient,QueryClientProvider  } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
@@ -12,6 +12,10 @@ const queryClient = new QueryClient({
   },
 });
 
+/**
+ * This provide access to the TanStack Query context,
+ * enabling hooks such as useQuery, useMutation, etc.
+ */
 export const TanStackQueryProvider = ({
   children,
 }: {
