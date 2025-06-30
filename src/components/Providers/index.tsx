64ed1d9f04e355763415feda75cc56419f17155a
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-06-30 16:32:57 -0700

    Fix file name

--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 
-import { TanStackQueryProvider } from "./tanStackQuery";
+import { TanStackQueryProvider } from "./TanStackQueryProvider";
 
 export default function Providers({ children }: { children: ReactNode }) {
   return <TanStackQueryProvider>{children}</TanStackQueryProvider>;
