24507ff975e4b531ec4da5c02d816fae05d5b250
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-06-30 17:42:38 -0700

    Ensure data freshness through client config

--- src/components/Providers/TanStackQueryProvider.tsx
+++ src/components/Providers/TanStackQueryProvider.tsx
@@ -7,14 +7,19 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      // This sets a good default for stale time on cached data
       staleTime: 60 * 1000,
+
+      // These ensure data freshness when returning to the relevant tab
+      refetchOnWindowFocus: true,
+      refetchOnReconnect: true,
     },
   },
 });
 
 /**
- * This provide access to the TanStack Query context,
- * enabling hooks such as useQuery, useMutation, etc.
+ * This provide access to the TanStack Query context, enabling hooks
+ * such as useQueryClient, useQuery, useMutation, etc.
  */
 export const TanStackQueryProvider = ({
   children,
