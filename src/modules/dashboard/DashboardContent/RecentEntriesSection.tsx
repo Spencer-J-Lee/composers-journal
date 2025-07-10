6114d541d5461946feed1eb4d6ac73ee41d5a5d3
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-07-10 14:31:57 -0700

    Improve recent entry loading, error, empty state

--- src/modules/dashboard/DashboardContent/RecentEntriesSection.tsx
+++ src/modules/dashboard/DashboardContent/RecentEntriesSection.tsx
@@ -1,25 +1,49 @@
 "use client";
 
+import { useEffect } from "react";
+
 import { CollapsibleSection } from "@/components/CollapsibleSection";
+import { ShimmerEntryCard } from "@/components/shimmerLoaders/ShimmerEntryCard";
+import { Typography } from "@/components/Typography";
 import { STATIC_TS_KEYS } from "@/constants/tanStackQueryKeys";
 import { useRecentlyUpdatedEntries } from "@/hooks/cache/entries";
 import { EntryCard } from "@/modules/search/components/EntryCard";
+import { showErrorToast } from "@/utils/client/toasts";
 
 export const RecentEntriesSection = () => {
-  const { data: entries, status, error } = useRecentlyUpdatedEntries();
+  const { data: entries, isPending, error } = useRecentlyUpdatedEntries();
 
-  // TODO: handle loading UI and error
-  if (status === "pending") return "Loading...";
-  if (error) {
-    console.error(error);
-  }
+  useEffect(() => {
+    if (error) {
+      console.error(error);
+      showErrorToast(error.message);
+    }
+  }, [error]);
 
-  if (!entries?.length) {
-    return null;
-  }
+  const renderContent = () => {
+    if (isPending) {
+      return (
+        <ul className="space-y-4">
+          <li>
+            <ShimmerEntryCard numControls={1} />
+          </li>
+        </ul>
+      );
+    }
 
-  return (
-    <CollapsibleSection title="Recent entries">
+    if (error) {
+      return (
+        <Typography variant="smallMuted">
+          Failed to load recent entries.
+        </Typography>
+      );
+    }
+
+    if (entries.length === 0) {
+      return <Typography variant="smallMuted">No recent entries</Typography>;
+    }
+
+    return (
       <ul className="space-y-4">
         {entries.map((entry) => (
           <li key={entry.id}>
@@ -31,6 +55,12 @@ export const RecentEntriesSection = () => {
           </li>
         ))}
       </ul>
+    );
+  };
+
+  return (
+    <CollapsibleSection title="Recent entries">
+      {renderContent()}
     </CollapsibleSection>
   );
 };
