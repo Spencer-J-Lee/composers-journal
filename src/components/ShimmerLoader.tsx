0c7a73fb760e84a2e7d55b025d73dd8f1f46158c
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-07-10 17:33:53 -0700

    Create shimmer button

--- src/components/ShimmerLoader.tsx
+++ src/components/ShimmerLoader.tsx
@@ -7,12 +7,14 @@ type ShimmerLoaderProps = {
   className?: string;
   typographyVariant?: TypographyVariant;
   fullWidth?: boolean;
+  insertText?: boolean;
 };
 
 export const ShimmerLoader = ({
   className,
   typographyVariant,
   fullWidth,
+  insertText,
 }: ShimmerLoaderProps) => {
   return (
     <div
@@ -22,6 +24,8 @@ export const ShimmerLoader = ({
         className,
       )}
     >
+      {insertText && <>&nbsp;</>}
+
       {typographyVariant && (
         <Typography
           variant={typographyVariant}
@@ -31,6 +35,7 @@ export const ShimmerLoader = ({
           &nbsp;
         </Typography>
       )}
+
       <div className="animate-shimmer-l-to-r absolute left-0 top-0 h-full w-full">
         <div className="via-shimmer-background h-full w-full bg-gradient-to-r from-transparent from-10% via-50% to-transparent to-80%" />
       </div>
---
+++ src/components/shimmerLoaders/ShimmerButton.tsx
@@ -0,0 +1,27 @@
+import clsx from "clsx";
+
+import { useButtonStyles } from "../buttons/hooks/useButtonStyles";
+import { ButtonSize } from "../buttons/types";
+import { ShimmerLoader } from "../ShimmerLoader";
+
+type ShimmerButtonProps = {
+  size: ButtonSize;
+  fullWidth?: boolean;
+  className?: string;
+};
+
+export const ShimmerButton = ({
+  size,
+  fullWidth,
+  className,
+}: ShimmerButtonProps) => {
+  const { buttonClassName } = useButtonStyles({
+    size,
+    fullWidth,
+    variant: "default",
+  });
+
+  return (
+    <ShimmerLoader className={clsx(buttonClassName, className)} insertText />
+  );
+};
