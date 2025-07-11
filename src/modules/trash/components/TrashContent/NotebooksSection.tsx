d2cfe8e6d71b47ea8eea1d31a9eaa0f6fd48455b
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-07-10 17:36:19 -0700

    Add loading, error, empty UI for trashed notebooks

--- src/modules/trash/components/TrashContent/NotebooksSection.tsx
+++ src/modules/trash/components/TrashContent/NotebooksSection.tsx
@@ -1,31 +1,65 @@
 import { CollapsibleSection } from "@/components/CollapsibleSection";
+import { ShimmerNotebookCard } from "@/components/shimmerLoaders/ShimmerNotebookCard";
+import { ShimmerSimpleFilters } from "@/components/shimmerLoaders/ShimmerSimpleFilters";
 import { SimpleFilters } from "@/components/SimpleFilters";
+import { Typography } from "@/components/Typography";
 import { useTrashedNotebooks } from "@/hooks/cache/notebooks";
 import { useSortedNotebooks } from "@/modules/notebooks/hooks/useSortedNotebooks";
 import { NotebookCard } from "@/modules/notebooks/list/NotebookCard";
+import { repeatRender } from "@/utils/client/repeatRender";
 
 export const NotebooksSection = () => {
-  const { data: notebooks } = useTrashedNotebooks();
+  const { data: notebooks, isPending, error } = useTrashedNotebooks();
   const { sortBy, setSortBy, sortedNotebooks } = useSortedNotebooks(notebooks);
 
-  if (!notebooks?.length) {
-    return null;
-  }
+  const renderContent = () => {
+    if (isPending) {
+      return (
+        <>
+          <ShimmerSimpleFilters className="mb-4" />
+
+          <ul className="space-y-4">
+            {repeatRender(3, (i) => (
+              <li key={i}>
+                <ShimmerNotebookCard controlsCount={2} />
+              </li>
+            ))}
+          </ul>
+        </>
+      );
+    }
+
+    if (error) {
+      return (
+        <Typography variant="smallMuted">
+          Failed to load trashed notebooks
+        </Typography>
+      );
+    }
+
+    if (notebooks.length === 0) {
+      return <Typography variant="smallMuted">No trashed notebooks</Typography>;
+    }
+
+    return (
+      <>
+        <SimpleFilters sortBy={sortBy} setSortBy={setSortBy} className="mb-4" />
+
+        <ul className="space-y-4">
+          {sortedNotebooks.map((notebook) => (
+            <li key={notebook.id}>
+              <NotebookCard
+                notebook={notebook}
+                controls={["restore", "delete"]}
+              />
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  };
 
   return (
-    <CollapsibleSection title="Notebooks">
-      <SimpleFilters sortBy={sortBy} setSortBy={setSortBy} className="mb-4" />
-
-      <ul className="flex flex-col gap-4">
-        {sortedNotebooks.map((notebook) => (
-          <li key={notebook.id}>
-            <NotebookCard
-              notebook={notebook}
-              controls={["restore", "delete"]}
-            />
-          </li>
-        ))}
-      </ul>
-    </CollapsibleSection>
+    <CollapsibleSection title="Notebooks">{renderContent()}</CollapsibleSection>
   );
 };
