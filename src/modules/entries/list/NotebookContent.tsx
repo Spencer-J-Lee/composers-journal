532e60348a378b60301c82fe40f093d2973ee18c
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-07-10 21:58:44 -0700

    Add loading, error, empty UI for entries

--- src/modules/entries/list/NotebookContent.tsx
+++ src/modules/entries/list/NotebookContent.tsx
@@ -4,13 +4,21 @@ import { Fragment, useEffect, useRef, useState } from "react";
 import clsx from "clsx";
 import debounce from "debounce";
 
+import { LinkButton } from "@/components/buttons/LinkButton";
 import { InformativeDivider } from "@/components/InformativeDivider";
 import { WorkspacePageWrapper } from "@/components/pageWrappers/WorkspacePageWrapper";
 import { WORKSPACE_WRAPPER_PX } from "@/components/pageWrappers/WorkspacePageWrapper/styles";
+import { ShimmerEntryCard } from "@/components/shimmerLoaders/ShimmerEntryCard";
+import { ShimmerSimpleFilters } from "@/components/shimmerLoaders/ShimmerSimpleFilters";
+import { StickyTopBar } from "@/components/StickyTopBar";
+import { Typography } from "@/components/Typography";
+import { routes } from "@/constants/routes";
 import { DYNAMIC_TS_KEYS } from "@/constants/tanStackQueryKeys";
 import { useInfEntryPages } from "@/hooks/cache/entries";
 import { Notebook } from "@/models/Notebook";
 import { EntryCard } from "@/modules/search/components/EntryCard";
+import { repeatRender } from "@/utils/client/repeatRender";
+import { showErrorToast } from "@/utils/client/toasts";
 
 import { DEFAULT_ENTRY_FILTER } from "./EntriesFilter/constants";
 import { EntryFilter } from "./EntriesFilter/types";
@@ -32,7 +40,8 @@ export const NotebookContent = ({ notebookId }: NotebookContentProps) => {
     hasNextPage,
     isFetching,
     isFetchingNextPage,
-    status,
+    isPending,
+    isSuccess,
   } = useInfEntryPages(notebookId, filters, offset);
   const isFetchingNextPageRef = useRef(isFetchingNextPage);
   const hasNextPageRef = useRef(hasNextPage);
@@ -59,6 +68,13 @@ export const NotebookContent = ({ notebookId }: NotebookContentProps) => {
     return () => container.removeEventListener("scroll", handleFetchTrigger);
   }, [fetchNextPage]);
 
+  useEffect(() => {
+    if (error) {
+      console.error(error);
+      showErrorToast(error.message);
+    }
+  }, [error]);
+
   const decrementOffset = () => {
     setOffset((prev) => prev - 1);
   };
@@ -68,39 +84,69 @@ export const NotebookContent = ({ notebookId }: NotebookContentProps) => {
     setFilters((prev) => prev);
   };
 
-  const renderContent = () => {
-    // TODO: handle loading UI and error
-    if (status === "pending") return "Loading...";
-    if (error) {
-      console.error(error);
-    }
-
-    // TODO: check if any entries exist for this notebook before showing empty
-    if (!data?.pages.length) {
-      return <NotebookEmptyState notebookId={notebookId} />;
-    }
-
+  if (isPending) {
     return (
-      <div className={clsx("py-4", WORKSPACE_WRAPPER_PX)}>
-        <ul className="space-y-4">
-          {data.pages.map(({ entries }, i) => (
-            <Fragment key={i}>
-              {entries.map((entry) => (
-                <li key={entry.id}>
-                  <EntryCard
-                    entry={entry}
-                    controls={["edit", "saving", "trash"]}
-                    queryKey={DYNAMIC_TS_KEYS.ENTRIES_BY_FILTERS(
-                      entry.notebookId,
-                      filters,
-                    )}
-                    onTrashSuccess={decrementOffset}
-                  />
-                </li>
-              ))}
-            </Fragment>
-          ))}
-        </ul>
+      <WorkspacePageWrapper paddingSize="none">
+        <StickyTopBar className="flex items-center justify-between">
+          <ShimmerSimpleFilters />
+          <LinkButton href={routes.entryCreate(notebookId)}>Create</LinkButton>
+        </StickyTopBar>
+
+        <div className={clsx("pb-4", WORKSPACE_WRAPPER_PX)}>
+          <ul className="space-y-4">
+            {repeatRender(3, (i) => (
+              <li key={i}>
+                <ShimmerEntryCard controlsCount={2} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </WorkspacePageWrapper>
+    );
+  }
+
+  if (isSuccess && data.pages.length === 0) {
+    return (
+      <WorkspacePageWrapper>
+        <NotebookEmptyState notebookId={notebookId} />;
+      </WorkspacePageWrapper>
+    );
+  }
+
+  return (
+    <WorkspacePageWrapper paddingSize="none" ref={containerRef}>
+      <StickyTopBar className="flex items-center justify-between">
+        {/* <SimpleFilters sortBy={sortBy} setSortBy={setSortBy} /> */}
+        <div>Filters placeholder</div>
+        <LinkButton href={routes.entryCreate(notebookId)}>Create</LinkButton>
+      </StickyTopBar>
+
+      <div className={clsx("pb-4", WORKSPACE_WRAPPER_PX)}>
+        {error && (
+          <Typography variant="smallMuted">Failed to load entries</Typography>
+        )}
+
+        {data && (
+          <ul className="space-y-4">
+            {data.pages.map(({ entries }, i) => (
+              <Fragment key={i}>
+                {entries.map((entry) => (
+                  <li key={entry.id}>
+                    <EntryCard
+                      entry={entry}
+                      controls={["edit", "saving", "trash"]}
+                      queryKey={DYNAMIC_TS_KEYS.ENTRIES_BY_FILTERS(
+                        entry.notebookId,
+                        filters,
+                      )}
+                      onTrashSuccess={decrementOffset}
+                    />
+                  </li>
+                ))}
+              </Fragment>
+            ))}
+          </ul>
+        )}
 
         {!hasNextPage && (
           <InformativeDivider className="mt-4">
@@ -111,12 +157,6 @@ export const NotebookContent = ({ notebookId }: NotebookContentProps) => {
         {/* TODO: handle loading UI */}
         <div>{isFetching ? "Fetching..." : null}</div>
       </div>
-    );
-  };
-
-  return (
-    <WorkspacePageWrapper paddingSize="none" ref={containerRef}>
-      {renderContent()}
     </WorkspacePageWrapper>
   );
 };
--- src/modules/entries/list/NotebookEmptyState.tsx
+++ src/modules/entries/list/NotebookEmptyState.tsx
@@ -1,5 +1,4 @@
 import { LinkButton } from "@/components/buttons/LinkButton";
-import { WorkspacePageWrapper } from "@/components/pageWrappers/WorkspacePageWrapper";
 import { ASSET_PATHS } from "@/constants/assetPaths";
 import { routes } from "@/constants/routes";
 
@@ -11,21 +10,19 @@ export type NotebookEmptyStateProps = {
 
 export const NotebookEmptyState = ({ notebookId }: NotebookEmptyStateProps) => {
   return (
-    <WorkspacePageWrapper>
-      <div className="flex min-h-full w-full items-center justify-center">
-        <EmptyState
-          imageData={{
-            src: ASSET_PATHS.EMPTY_STATE_WRITING,
-            alt: "Cartoon character writing lines with giant pencil.",
-          }}
-          title="No entries yet"
-          description="Start by creating a new one"
-        >
-          <LinkButton href={routes.entryCreate(notebookId)}>
-            Get Started
-          </LinkButton>
-        </EmptyState>
-      </div>
-    </WorkspacePageWrapper>
+    <div className="flex min-h-full w-full items-center justify-center">
+      <EmptyState
+        imageData={{
+          src: ASSET_PATHS.EMPTY_STATE_WRITING,
+          alt: "Cartoon character writing lines with giant pencil.",
+        }}
+        title="No entries yet"
+        description="Start by creating a new one"
+      >
+        <LinkButton href={routes.entryCreate(notebookId)}>
+          Get Started
+        </LinkButton>
+      </EmptyState>
+    </div>
   );
 };
