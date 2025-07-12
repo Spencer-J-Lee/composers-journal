81cd367974df096fa05ef54ea5bc80bbb88386d1
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-07-11 21:23:29 -0700

    Make circle loader for flexible in color

--- src/components/loaders/CircleLoader.tsx
+++ src/components/loaders/CircleLoader.tsx
@@ -1,5 +1,5 @@
 export const CircleLoader = () => {
   return (
-    <div className="border-r-accent h-10 w-10 animate-spin rounded-full border-4 border-[#444548]" />
+    <div className="border-accent h-10 w-10 animate-spin rounded-full border-4 border-r-transparent" />
   );
 };
