04005297aeece1a5b76c07aee3d0a3f2e149ee4d
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-07-16 13:20:46 -0700

    Fix broken UI

--- src/components/SkipToContent.tsx
+++ src/components/SkipToContent.tsx
@@ -6,7 +6,8 @@ export const SkipToContent = () => {
   return (
     <LinkButton
       href={`#${ELEMENT_IDS.MAIN_CONTENT}`}
-      className="z-skip-to-content absolute left-1/2 top-0 min-w-max -translate-x-1/2 -translate-y-full rounded-t-none opacity-0 transition-all focus-visible:translate-y-0 focus-visible:opacity-100"
+      className="z-skip-to-content left-1/2 top-0 min-w-max -translate-x-1/2 -translate-y-full rounded-t-none opacity-0 transition-all focus-visible:translate-y-0 focus-visible:opacity-100"
+      style={{ position: "absolute" }}
     >
       Skip to Content
     </LinkButton>
