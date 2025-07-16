5ff54116440645cd6b4da429fef858c036b769f9
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-07-16 13:54:37 -0700

    Add TODOs

--- src/constants/assetPaths.ts
+++ src/constants/assetPaths.ts
@@ -1,6 +1,9 @@
 export const ASSET_PATHS = {
   LOGO_WHITE: "/assets/logo-white.png",
   LOGO_BLACK: "/assets/logo-black.png",
-  EMPTY_STATE_WRITING: "/assets/empty-state-writing.png", // TODO: move this to an outside image storage like amazon s3
-  EMPTY_STATE_TRASH: "/assets/empty-state-trash.png", // TODO: move this to an outside image storage like amazon s3
+
+  // TODO: move to image storage (e.g. amazon s3) to reduce bundle size
+  // TODO: replace these illustrations
+  EMPTY_STATE_WRITING: "/assets/empty-state-writing.png",
+  EMPTY_STATE_TRASH: "/assets/empty-state-trash.png",
 } as const;
