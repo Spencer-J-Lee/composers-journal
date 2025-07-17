5953bdbb8400f8c2dc5ef79909b7f77e578e4ac3
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-07-16 19:02:33 -0700

    Implement empty state for tags editor

--- src/modules/entries/components/EntryForm/TagsEditor.tsx
+++ src/modules/entries/components/EntryForm/TagsEditor.tsx
@@ -3,6 +3,7 @@ import clsx from "clsx";
 
 import { Divider } from "@/components/Divider";
 import { IconButton } from "@/components/iconButtons/IconButton";
+import { Typography } from "@/components/Typography";
 import { Tag } from "@/models/Tag";
 import { TagChips } from "@/modules/tags/components/TagChips";
 
@@ -18,10 +19,14 @@ export const TagsEditor = ({ tags, className }: TagsEditorProps) => {
 
       <Divider orientation="vertical" flexChild className="ml-2 mr-4" />
 
-      <TagChips
-        tags={tags}
-        className="flex-overflow-fix hide-scrollbar -m-4 overflow-x-auto text-nowrap p-4"
-      />
+      {tags.length < 0 ? (
+        <TagChips
+          tags={tags}
+          className="flex-overflow-fix hide-scrollbar -m-4 flex-1 overflow-x-auto text-nowrap p-4"
+        />
+      ) : (
+        <Typography variant="emptyState">No tags</Typography>
+      )}
     </div>
   );
 };
