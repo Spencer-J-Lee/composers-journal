ea2b57966a1dbc82a13c303857af8992af0e4715
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-07-30 14:51:38 -0700

    Add link to notebook from card

--- src/modules/notebooks/list/NotebookCard/NotebookInfo.tsx
+++ src/modules/notebooks/list/NotebookCard/NotebookInfo.tsx
@@ -1,4 +1,7 @@
+import Link from "next/link";
+
 import { Typography } from "@/components/Typography";
+import { routes } from "@/constants/routes";
 import { Notebook } from "@/models/Notebook";
 
 type NotebookInfoProps = {
@@ -9,10 +12,13 @@ export const NotebookInfo = ({ notebook }: NotebookInfoProps) => {
   const { name, createdAt } = notebook;
 
   return (
-    <div className="border-border flex flex-1 flex-col border-l p-4">
-      <Typography variant="h4" className="mb-1">
-        {name}
-      </Typography>
+    <div className="border-border flex flex-1 flex-col items-start border-l p-4">
+      <Link
+        href={routes.notebook(notebook.id)}
+        className="mb-1 hover:underline"
+      >
+        <Typography variant="h4">{name}</Typography>
+      </Link>
 
       <div className="mt-auto">
         <Typography variant="smallMuted">
