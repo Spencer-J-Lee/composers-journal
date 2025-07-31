1e5013f9be21c88e43b5e9b035f62200af9fabf5
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-07-31 15:17:07 -0700

    Fix recent entries section grabbing trashed entries

--- src/constants/entryParams.ts
+++ src/constants/entryParams.ts
@@ -4,5 +4,5 @@ import { apiGetEntriesProps } from "@/services/entries/get";
 export const RECENTLY_UPDATED_ENTRIES_PARAMS: apiGetEntriesProps = {
   orderBy: [{ column: "updatedAt", direction: "desc" }],
   limit: 3,
-  status: STATUSES.TRASHED,
+  status: STATUSES.ACTIVE,
 };
