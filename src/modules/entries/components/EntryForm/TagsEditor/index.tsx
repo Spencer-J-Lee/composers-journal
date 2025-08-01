af33d64fd731d5c61cb06b573ee099e0790af4d7
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-08-01 09:52:06 -0700

    Fix tag display logic

--- src/modules/entries/components/EntryForm/TagsEditor/index.tsx
+++ src/modules/entries/components/EntryForm/TagsEditor/index.tsx
@@ -19,7 +19,7 @@ export const TagsEditor = ({ tags, className }: TagsEditorProps) => {
 
       <Divider orientation="vertical" flexChild className="ml-2 mr-4" />
 
-      {tags.length < 0 ? (
+      {tags.length > 0 ? (
         <TagChips
           tags={tags}
           className="flex-overflow-fix hide-scrollbar -m-4 flex-1 overflow-x-auto text-nowrap p-4"
