14795116f876b7123edde476b4e52cef1eceda1c
Spencer-J-Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2025-08-06 11:13:10 -0700

    Implement entry tag edit functionality

--- src/modules/entries/edit/EditEntryForm.tsx
+++ src/modules/entries/edit/EditEntryForm.tsx
@@ -4,12 +4,16 @@ import { useRouter } from "next/navigation";
 
 import { SUCCESS_MESSAGES } from "@/constants/messages";
 import { routes } from "@/constants/routes";
+import { useCreateTags } from "@/hooks/cache/tags";
 import { Entry } from "@/models/Entry";
 import { apiUpdateEntry } from "@/services/entries/update";
+import { apiCreateEntryTags } from "@/services/entryTags/create";
+import { apiDeleteEntryTags } from "@/services/entryTags/delete";
 import { showSuccessToast } from "@/utils/client/toasts";
 
 import { EntryForm } from "../components/EntryForm";
 import { EntryFormValues } from "../components/EntryForm/schema";
+import { tagsToOptions } from "../components/EntryForm/TagsEditor/TagsDialog/helpers";
 
 type EditEntryFormProps = {
   entry: Entry;
@@ -17,15 +21,51 @@ type EditEntryFormProps = {
 
 export const EditEntryForm = ({ entry }: EditEntryFormProps) => {
   const router = useRouter();
+  const { mutateAsync: createTags } = useCreateTags();
 
   const handleSubmit = async (data: EntryFormValues) => {
-    await apiUpdateEntry(entry.id, data);
+    const { tagOptions, ...rest } = data;
 
-    // TODO: handle Tag update
-    // TODO: handle entryTag update
+    // Split tags into existing and new
+    const newTagOptions = tagOptions.filter(({ isNew }) => isNew);
+    const existingTagIds = tagOptions
+      .filter(({ isNew }) => !isNew)
+      .map(({ value }) => value);
+
+    // Create any new tags
+    const newTags =
+      newTagOptions.length > 0
+        ? await createTags(newTagOptions.map(({ label }) => ({ name: label })))
+        : [];
+    const selectedTagIds = [...existingTagIds, ...newTags.map((tag) => tag.id)];
+
+    // Compute which old tags to remove
+    const prevTagIds = entry.tags.map((tag) => tag.id);
+    const tagIdsToRemove = prevTagIds.filter(
+      (id) => !selectedTagIds.includes(id),
+    );
+
+    // Compute which tags to add
+    const tagIdsToAdd = selectedTagIds.filter((id) => !prevTagIds.includes(id));
+
+    await Promise.all([
+      apiUpdateEntry(entry.id, rest),
+
+      tagIdsToRemove.length > 0
+        ? apiDeleteEntryTags({
+            entryId: entry.id,
+            ids: tagIdsToRemove,
+          })
+        : Promise.resolve(),
+
+      tagIdsToAdd.length > 0
+        ? apiCreateEntryTags(
+            tagIdsToAdd.map((tagId) => ({ entryId: entry.id, tagId })),
+          )
+        : Promise.resolve([]),
+    ]);
 
     showSuccessToast(SUCCESS_MESSAGES.USER.EDIT.ENTRY);
-
     router.push(routes.notebook(entry.notebookId));
   };
 
@@ -36,7 +76,7 @@ export const EditEntryForm = ({ entry }: EditEntryFormProps) => {
       defaultValues={{
         title: entry.title,
         description: entry.description,
-        tags: entry.tags,
+        tagOptions: tagsToOptions(entry.tags),
       }}
     />
   );
